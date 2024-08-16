4134890cd3a470e4300157a84a60078b41df14e6
PRathod27 <prem.rathod.2715@gmail.com>
2024-08-16 23:08:35 +0530

    first commit

---
+++ app/api/generate/route.js
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const systemPrompt = `You are a flashcard creator.
+
+1. Your task is to generate flashcards based on the provided text.
+2. Each flashcard should have a question on one side and the answer on the other side.
+3. Ensure that the questions are clear and concise.
+4. The answers should be accurate and relevant to the questions.
+5. If the text contains multiple concepts, create multiple flashcards.
+6. Use simple language that is easy to understand.
+7. Format the flashcards in a JSON array with each flashcard as an object containing 'question' and 'answer' keys.
+8. Avoid using technical jargon unless necessary.
+9. Make sure the flashcards cover all key points from the text.
+10. Review the flashcards for any grammatical or spelling errors.
+
+Return in the following JSON Format
+{
+    "flashcards": [{
+        "front": str,
+        "back": str
+}]
+}
+`;
+
+
+export async function POST (req) {
+    const openai = OpenAI();
+    const data  = await req.text();
+
+    const completion = await openai.chat.completion.create(
+        {
+            messages:[{role: "system", content : systemPrompt}, {role: "user", content: data}], 
+            model : 'gpt-3.5-turbo',
+            respone_format:{type:'json_object'}
+        }  
+    )
+
+    const flashCards = JSON.parse(completion.choices[0].message.content);
+
+    return NextResponse.json({flashCards});
+}
---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC3_whvDZhe5Gp1ykngjC-owQKORqlfP4g",
+  authDomain: "flashcard-saas-f8883.firebaseapp.com",
+  projectId: "flashcard-saas-f8883",
+  storageBucket: "flashcard-saas-f8883.appspot.com",
+  messagingSenderId: "477891484994",
+  appId: "1:477891484994:web:210689ed6253e9619a3cdb",
+  measurementId: "G-KSB3S596R3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
